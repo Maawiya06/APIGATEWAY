3010528a61ebb16bacc32b161a30d59d5c2bbeef
Maawiya06 <maawiyaameer001@gmail.com>
2026-02-05 22:17:22 +0530

    commit

---
+++ src/main/java/com/lcwd/gateway/configuration/securityConfig.java
@@ -0,0 +1,9 @@
+package com.lcwd.gateway.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+@Configuration
+@EnableWebFluxSecurity
+public class securityConfig {
+}
