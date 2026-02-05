3f0703d5962b127f961351f0898bf71473b62019
Maawiya06 <maawiyaameer001@gmail.com>
2026-02-06 00:38:18 +0530

    commit

--- src/main/java/com/lcwd/gateway/controller/AuthController.java
+++ src/main/java/com/lcwd/gateway/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.lcwd.gateway.controller;
 
+import com.lcwd.gateway.models.AuthResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -15,5 +16,7 @@ public class AuthController {
 
     @GetMapping("login")
     public ResponseEntity<AuthResponse>login(){
+
+        return new ResponseEntity<>();
     }
 }
